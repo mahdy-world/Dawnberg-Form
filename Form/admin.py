8a0eac6fe4cab9ec4e8b249c1f00aff6fa6485ba
Ahmed El-Khayyat <ahmed.elkhayyat@onlink4it.com>
2021-08-09 18:48:49 +0200

    elkhayyat update at 9-8-2021

--- Form/admin.py
+++ Form/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Form , Question , QuestionOption
+from .models import *
+
 # Register your modeladmion
 admin.site.register(Form)
 admin.site.register(Question)
 admin.site.register(QuestionOption)
+admin.site.register(Answer)
--- Form/models.py
+++ Form/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 
 class Form(models.Model):
-    title = models.CharField(max_length=50,)
-    description = models.CharField(max_length=100,)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE , verbose_name = 'انشئت بواسطة')
-    created_at = models.DateTimeField(auto_now=True,  verbose_name='تاريخ الانشاء ')
+    title = models.CharField(max_length=50, )
+    description = models.CharField(max_length=100, )
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='انشئت بواسطة')
+    created_at = models.DateTimeField(auto_now=True, verbose_name='تاريخ الانشاء ')
     deleted = models.BooleanField(default=False, verbose_name='حذف')
 
     def __str__(self):
@@ -17,22 +18,37 @@ class Form(models.Model):
 
 class Question(models.Model):
     question_type = (
-        ('نص قصير','نص قصير'),
-        ('نص طويل','نص طويل'),
-        ('اختيار متعدد','اختيار متعدد '),
-        ('واحد من متعدد ','واحد من متعدد')
+        (1, 'نص قصير'),
+        (2, 'نص طويل'),
+        (3, 'اختيار متعدد '),
+        (4, 'واحد من متعدد')
     )
-    form = models.ForeignKey(Form,verbose_name="المنوذج", on_delete=models.CASCADE) 
-    question_name= models.CharField(max_length=50,verbose_name='عنوان السؤال')
-    question_type=models.CharField(max_length=50, choices=question_type)  
-    
+    form = models.ForeignKey(Form, verbose_name="المنوذج", on_delete=models.CASCADE)
+    question_name = models.CharField(max_length=50, verbose_name='عنوان السؤال')
+    question_type = models.IntegerField(choices=question_type)
+
     def __str__(self):
         return self.question_name
 
 
 class QuestionOption(models.Model):
-    question = models.ForeignKey(Question, verbose_name="السؤال", on_delete=models.CASCADE) 
-    option = models.CharField( max_length=50 , verbose_name='الاختيار')       
+    question = models.ForeignKey(Question, verbose_name="السؤال", on_delete=models.CASCADE)
+    option = models.CharField(max_length=50, verbose_name='الاختيار')
 
     def __str__(self):
         return self.option
+
+
+class AnswerPerson(models.Model):
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    person = models.ForeignKey(AnswerPerson, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, verbose_name='السؤال', on_delete=models.CASCADE)
+    text_answer = models.TextField(verbose_name='الإجابة')
+    optional_answer = models.ManyToManyField(QuestionOption, verbose_name='الإجابة')
+
+    def __str__(self):
+        return str(self.id)
